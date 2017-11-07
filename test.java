6b564a00cdc9fd2d32e1ed8c0b801eac06bc5123
Mohamed Hojeij <hoojej@gmail.com>
2017-11-07 21:06:54 +0100

    Added support for printing tags

--- test.java
+++ test.java
@@ -3,4 +3,7 @@
             public static void main(String[] args){
                     System.out.println("This is now changed");
             }
-    }
+ 
+public static void printWithTag(String tag, String message){
+        System.out.println("["+ tag +"] " + message);
+}   }
